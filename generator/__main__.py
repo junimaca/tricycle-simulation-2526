308f75b31ea242c82b5fdbbee9279ffd14409066
junimaca <leejustinemaca@gmail.com>
2026-01-29 10:22:01 +0800

    Chaged parameters of number of trikes and passengers for testing

--- generator/__main__.py
+++ generator/__main__.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     # these are the basic parameters if you want to
     # simulate simple non-roaming cases. For more complex
     # cases, you would need to consult the code in more details
-    NUM_TRIKES = 15
+    NUM_TRIKES = 1
     NUM_TERMINALS = 2
-    NUM_PASSENGERS = 200
+    NUM_PASSENGERS = 20
     MAX_TIME = 5_000 
     TEST_COUNT = 1
     
