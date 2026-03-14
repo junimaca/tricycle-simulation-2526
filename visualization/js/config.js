7582fd9f09557019e8657cf478fb5fa49643f1aa
Zach <zachary.nabong10@gmail.com>
2026-03-14 17:07:38 +0800

    changed id for testing

--- visualization/js/config.js
+++ visualization/js/config.js
@@ -30,7 +30,7 @@ export const API_ENDPOINTS = {
 
 // Default simulation parameters
 export const DEFAULT_SIMULATION = {
-    id: "1-1-20-ryavecmeofpz",
+    id: "1-1-20-cencnbzjjnnk",
     trikes: 1,
     passengers: 20
 };
